bd6caa7729265a1848f9277fd6bfc5e5233f76aa
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-03-23 22:33:39 +0530

    posts added to home page

--- home/views.py
+++ home/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
+from account.models import User, Post
+from task.models import TaskTodo
+
 # Create your views here.
 def home(request):
     username = request.session.get('username', None)
+    posts = Post.objects.all()
     context = {
         'isLoggedin': True if username else False,
         'username': username,
-
+        'posts': posts,
+        
     }
     return render(request, 'home.html', context)
 
