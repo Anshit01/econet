8d44c32ad97f6d06d73b633ac8041536b7f6f3dd
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-03-23 21:37:53 +0530

    navbar updated

--- task/views.py
+++ task/views.py
@@ -4,11 +4,14 @@ from django.http import HttpResponse
 from task.models import TaskTodo
 
 def taskHome(request):
+    username = request.session.get('username', None)
     allTask = TaskTodo.objects.all()
     generalTasks = TaskTodo.objects.filter(taskType="G")
     weeklyTasks = TaskTodo.objects.filter(taskType="W")
-
+    
     data = {
+        'isLoggedin': True if username else False,
+        'username': username,
         "allTasks":allTask,
         "generalTasks": generalTasks,
         "weeklyTasks" : weeklyTasks
--- userProfile/views.py
+++ userProfile/views.py
@@ -4,10 +4,13 @@ from account.models import User, Post
 
 # Create your views here.
 def user(request, username):
+    username = request.session.get('username', None)
     users = User.objects.filter(username=username)
     if users.count():
         context = {
-            'user': users[0]
+            'user': users[0],
+            'isLoggedin': True if username else False,
+            'username': username,
         }
         return render(request, 'profile.html', context)
     return redirect('/')
