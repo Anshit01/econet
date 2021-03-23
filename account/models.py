fc7e03d02fdaac79245f6aa7c05338f2cf034c41
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-03-23 10:52:43 +0530

    made user and posts model

--- account/models.py
+++ account/models.py
@@ -1,40 +1,52 @@
 from django.db import models
+from djongo import models
 
 from tasks.models import Task
 
 # Create your models here.
+
+
 class User(models.Model):
     username = models.CharField(max_length=100)
-    imageURL = models.URLField()
+    imageURL = models.URLField(default="https://i.imgur.com/4zsBXBE.png")
     bio = models.TextField()
 
     #stats
     badge = models.CharField(max_length=100)
     points = models.IntegerField()
-    rank = models.IntegerField()
 
     tasksCompleted = models.ArrayReferenceField(
         to=Task,
-        on_delete=models.CASCADE
+        on_delete=models.CASCADE,
+        blank=True
     )
     
-    posts = models.ArrayReferenceField(
-        to=Post,
-        on_delete=models.CASCADE
-    )
+    # posts: reverse reference
 
     def __str__(self):
-        return self.username
+        return str(self.id) + ' - ' + self.username + ' - ' + str(len(self.posts.all()))
 
 class Post(models.Model):
     
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+
     caption = models.TextField()
     imageURL = models.URLField()
-    
+
     linkedTask = models.ForeignKey(
         to=Task,
         on_delete=models.CASCADE,
-        null=True,
+        related_name='posts',
         blank=True
     )
+    #tags
+
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.author)
 
