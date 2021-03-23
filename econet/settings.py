b2d63552dce0fa15897f2c8e9fef72b847badc4c
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-03-23 19:45:02 +0530

    fixed collect static bug

--- econet/settings.py
+++ econet/settings.py
@@ -138,10 +138,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'app','static'),
-]
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
