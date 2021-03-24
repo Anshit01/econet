03153212b99bf343a495388957c0d55e18cefbcd
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-03-24 22:25:32 +0530

    login

--- account/views.py
+++ account/views.py
@@ -4,22 +4,33 @@ from .models import User
 
 # Create your views here.
 def register(request):
+    context = {
+        'error': ''
+    }
     if request.method == 'GET':
         print(request.session.get('username', None))
-        return render(request, 'register.html')
+        return render(request, 'register.html', context)
 
     elif request.method == 'POST':
-        #TODO
-        pass
+        data = request.POST
+        print(data)
+        return render(request, 'register.html', context)
 
 def login(request):
+    context = {
+        'error': ''
+    }
     if request.method == 'GET':
-        request.session['username'] = 'anshit'
-        return render(request, 'login.html')
+        # request.session['username'] = 'anshit'
+        return render(request, 'login.html', context)
 
     elif request.method == 'POST':
-        #TODO
-        pass
+        data = request.POST
+        print(data)
+        username = data['username']
+        password = data['password']
+        print(username, password)
+        return render(request, 'login.html', context)
 
 def logout(request):
     request.session.pop('username', None)
