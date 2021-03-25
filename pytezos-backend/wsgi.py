728b67f55152353eface1f27bd1cd93437cb3ba6
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-03-26 02:25:45 +0530

    .

--- pytezos-backend/wsgi.py
+++
@@ -1,8 +0,0 @@
-from app.main import app
-from app.config import config
-
-if __name__ == "__main__":
-    debug = False
-    if 'DEBUG' in config and config['DEBUG']:
-        debug = True
-    app.run(debug=debug)
