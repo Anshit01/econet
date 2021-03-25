a442f166bfd5f2fe325f7282211c366e6764bc8e
shwetalsoni <sonishwetal704@gmail.com>
2021-03-25 17:03:41 +0530

    .

--- task/views.py
+++ task/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from account.models import User
 from task.models import TaskTodo
 
 def taskHome(request):
@@ -8,13 +8,16 @@ def taskHome(request):
     allTask = TaskTodo.objects.all()
     generalTasks = TaskTodo.objects.filter(taskType="G")
     weeklyTasks = TaskTodo.objects.filter(taskType="W")
+    users = User.objects.filter(username=username)
     
     data = {
         'isLoggedin': True if username else False,
         'username': username,
         "allTasks":allTask,
         "generalTasks": generalTasks,
-        "weeklyTasks" : weeklyTasks
+        "weeklyTasks" : weeklyTasks,
     }
+    if users.count():
+        data["user"] = users[0]
 
     return render(request, "task.html", data)
